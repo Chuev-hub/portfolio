adedfd25a9f5587072e3b549dfebc9b6f7ab2479
ilya chuev <chuev120@gmail.com>
2022-04-27 23:03:05 +0200

    First real commit. StartMainPage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,12 @@ import '../index.css'
 function Home(){
   return (
        <div >
-       <h3>SAAA</h3>
+       <div className="d-flex justify-content-center">
+       <div className="d-flex  flex-column">
+       <div><p className=" " style={{fontSize:"8vw",marginTop:"20vw",marginBottom:"0"}}>CHUEV ILYA</p>
+       </div><div><p className=" " style={{fontSize:"3vw",marginLeft:"1vw", color:"#5cff72"}}>Fullstack developer</p>
+       </div></div>
+        </div>
         </div>
   );
 }
