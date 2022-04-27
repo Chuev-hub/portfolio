8670f1c1de77f515924f7f16e84732e3653f2c25
ilya chuev <chuev120@gmail.com>
2022-04-27 23:50:55 +0200

    lite commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home(){
        <div >
        <div className="d-flex justify-content-center">
        <div className="d-flex  flex-column">
-       <div><p className=" " style={{fontSize:"8vw",marginTop:"20vw",marginBottom:"0"}}>CHUEV ILYA</p>
+       <div><p className=" " style={{fontSize:"8vw",marginTop:"20vw",marginBottom:"0"}}>CHUEV ILYA.</p>
        </div><div><p className=" " style={{fontSize:"3vw",marginLeft:"1vw", color:"#5cff72"}}>Fullstack developer</p>
        </div></div>
         </div>
