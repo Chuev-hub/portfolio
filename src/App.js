23fdb4a5af7dba55ab04108b574b66aaa5a2dc43
ilya chuev <chuev120@gmail.com>
2022-04-27 17:39:59 +0200

    1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return ( < div className = "App" >
         <header className = "App-header" >
-        оиіааиіоиітвиьтівиаівиатвіит </header> </div >
+        маша красотка </header> </div >
     );
 }
 
