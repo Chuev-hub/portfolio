1a753bd0650456757664ac7065ea0caf919092ad
ilya chuev <chuev120@gmail.com>
2022-04-27 20:40:00 +0200

    1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (<  >
     <div className='d-flex justify-content-center'>
-         <input type={"text"} style = {{textAlign:"center", marginTop:"50px"}} value={"слободян лох"}></input> 
+         <input type={"text"} style = {{textAlign:"center", marginTop:"50px"}} value={"файна лох"}></input> 
          </div>
        </ >
     );
