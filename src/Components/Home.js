7aa624d0758238d6f9444501c6bf51fb9947a63a
ilya chuev <chuev120@gmail.com>
2022-04-29 00:19:24 +0200

    .

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,7 @@ function Home() {
           <div><p className="name" >CHUEV ILYA</p>
           </div><div className="wrapper">
           <Typed 
-                    strings={['<span class="title">Full-stack developer</span><br/><span class="title3">.NET + React</span><br/><span class="title2">scroll down for my portfolio ˅</span>']}
+                    strings={['<span class="title">Full-stack developer</span><br/><span class="title3">.NET + React</span><br/><span class="title2">scroll down for my portfolio˅</span>']}
                     typeSpeed={60}
                 />
                 <div>
