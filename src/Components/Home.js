5b5c586f45276ce3da2a5f61f2de6b6ac439e7e3
ilya chuev <chuev120@gmail.com>
2022-04-29 00:16:41 +0200

    start media

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../index.css'
-function Home(){
+import { useMediaQuery } from 'react-responsive'
+import Typed from 'react-typed';
+function Home() {
+  
   return (
-       <div >
-       <div className="d-flex justify-content-center">
-       <div className="d-flex  flex-column">
-       <div><p className=" " style={{fontSize:"8vw",marginTop:"20vw",marginBottom:"0"}}>CHUEV ILYA.</p>
-       </div><div><p className=" " style={{fontSize:"3vw",marginLeft:"1vw", color:"#5cff72"}}>Fullstack developer</p>
-       </div></div>
-        </div>
-        </div>
+    <div >
+     
+     
+     <div className="d-flex justify-content-center">
+        <div className="d-flex  flex-column">
+          <div><p className="name" >CHUEV ILYA</p>
+          </div><div className="wrapper">
+          <Typed 
+                    strings={['<span class="title">Full-stack developer</span><br/><span class="title3">.NET + React</span><br/><span class="title2">scroll down for my portfolio ˅</span>']}
+                    typeSpeed={60}
+                />
+                <div>
+                </div>
+          </div></div>
+      </div>
+    </div>
   );
 }
-  
+
 export default Home;
