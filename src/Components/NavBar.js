97f687cb3702a38a9f380c628ef87ee24666c6e2
ilya chuev <chuev120@gmail.com>
2022-05-06 17:15:36 +0200

    в

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function NavBar(){
 return(
-    <nav className="navbar navbar-expand-lg navbar-dark  "style={{backgroundColor: "#61dafb00"}}>
+    <nav className="navbar navbar-default navbar-dark"style={{backgroundColor: "#61dafb00"}}>
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">Home</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-     
-      <form className="d-flex">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
         <Link className="nav-link active" to="/">Projects</Link>
         </li>
@@ -19,7 +17,8 @@ return(
         <Link className="nav-link " to="/">Contact</Link>
         </li>
       
-      </ul></form>
+      </ul>
+     
     </div>
   </div>
 </nav>
