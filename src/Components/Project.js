5111dd4b5846d659ea5e1bdf41788bd9a4eabc95
ilya chuev <chuev120@gmail.com>
2022-07-03 19:12:09 +0200

    .

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,10 @@
 import React from "react";
 function Project(props) {
   
-    return (
-        <div>{props.match.params.id}</div>
+    return (<div>
+
+        <div>{props.match.params.id}</div> 
+        <div>{props.match.params.technology}</div> 
+        </div>
     );}
     export default Project;
