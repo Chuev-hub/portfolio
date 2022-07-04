e125fe503eeac9094a823ec09781978a316c131e
ilya chuev <chuev120@gmail.com>
2022-07-04 22:31:31 +0200

    й

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,24 +1,21 @@
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 function Card(props) {
-  
-    return (
-        <div className="card border-light text-center mt-3 mb-4" style={{background:"rgba(0, 0, 0, 0.2)",width: '400px'}}>
-  <div className="card-header">
-  {props.technology}
-  </div>
-  <div className="card-body ">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.options}</p>
-    <Link to={props.link} className="btn btn-light">View</Link>
-  </div>
-  <div className="card-footer text-light">
-  {props.date}
-  </div>
-</div>
-    );}
-    export default Card;
-
-  
+  return (
+    <div
+      className="card border-light text-center mt-3 mb-4"
+      style={{ background: "rgba(0, 0, 0, 0.2)", width: "400px" }}
+    >
+      <div className="card-header">{props.technology}</div>
+      <div className="card-body ">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.options}</p>
+        <Link to={props.link} className="btn btn-light">
+          View
+        </Link>
+      </div>
+      <div className="card-footer text-light">{props.date}</div>
+    </div>
+  );
+}
+export default Card;
